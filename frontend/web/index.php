516e23aad5ef42db67cbf390db1df1f64772d8c5
admin <iskakov_zhanat@mail.ru>
2014-03-02 18:51:49 +0600

    redirect

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -1,7 +1,8 @@
 <?php
 
-phpinfo();
-exit;
+if (strpos($_SERVER['REQUEST_URI'], 'admin') !== FALSE) {
+    header("Location: http://calm-bayou-1695.herokuapp.com//backend/web/index.php");
+}
 
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
